8f0230a4e7492e0aaf374d98afca1e56745e0d94
Galibier <cx.sjtu@gmail.com>
2020-04-17 00:42:53 +0800

    [30] Substring with Concatenation of All Words

--- LeetCode/cpp/30.substring-with-concatenation-of-all-words.cpp
+++ LeetCode/cpp/30.substring-with-concatenation-of-all-words.cpp
@@ -7,6 +7,46 @@
 // @lc code=start
 class Solution {
  public:
-  vector<int> findSubstring(string s, vector<string>& words) {}
+  vector<int> findSubstring(string s, vector<string>& words) {
+    vector<int> ans;
+    int len = s.size(), cnt = words.size();
+    if (len == 0 || cnt == 0) return ans;
+
+    unordered_map<string, int> dict;
+    for (auto word : words) dict[word]++;
+
+    int width = words[0].size();
+    for (int i = 0; i < width; i++) {
+      int left = i, count = 0;
+      unordered_map<string, int> tdict;
+      for (int j = i; j <= len - width; j += width) {
+        auto str = s.substr(j, width);
+        if (dict.count(str)) {
+          tdict[str]++;
+          if (tdict[str] <= dict[str])
+            count++;
+          else {
+            while (tdict[str] > dict[str]) {
+              auto str1 = s.substr(left, width);
+              tdict[str1]--;
+              if (tdict[str1] < dict[str1]) count--;
+              left += width;
+            }
+          }
+          if (count == cnt) {
+            ans.push_back(left);
+            tdict[s.substr(left, width)]--;
+            count--;
+            left += width;
+          }
+        } else {
+          tdict.clear();
+          count = 0;
+          left = j + width;
+        }
+      }
+    }
+    return ans;
+  }
 };
 // @lc code=end
