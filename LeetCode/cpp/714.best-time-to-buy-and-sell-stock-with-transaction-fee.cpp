e984b626bcaec2950201077c0442d617f271e79f
Galibier <cx.sjtu@gmail.com>
2020-04-20 00:08:14 +0800

    [714] Best Time to Buy and Sell Stock with Transaction Fee

---
+++ LeetCode/cpp/714.best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=714 lang=cpp
+ *
+ * [714] Best Time to Buy and Sell Stock with Transaction Fee
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int maxProfit(vector<int>& prices, int fee) {
+    int n = prices.size();
+    // vector<vector<int>> dp(n, vector<int>(2, 0));
+
+    // dp[0][0] = 0;
+    // dp[0][1] = INT_MIN;
+    int dp_i_0 = 0, dp_i_1 = INT_MIN;
+    for (int i = 0; i < n; i++) {
+      int tmp = dp_i_0;
+      //   dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+      dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+      //   dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+      dp_i_1 = max(dp_i_1, tmp - prices[i] - fee);
+    }
+
+    // return dp[n - 1][0];
+    return dp_i_0;
+  }
+};
+// @lc code=end
