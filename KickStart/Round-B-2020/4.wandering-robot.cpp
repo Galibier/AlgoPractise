3c5a53c3e54c98b4baebe5a9d00706d85ff93f79
Galibier <cx.sjtu@gmail.com>
2020-04-20 23:58:32 +0800

    remaining

--- KickStart/Round-B-2020/4.wandering-robot.cpp
+++ KickStart/Round-B-2020/4.wandering-robot.cpp
@@ -1,38 +1,75 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <bits/stdc++.h>
 
-int uniquePaths(int m, int n) {
-  vector<vector<int>> path(m, vector<int>(n, 1));
-  for (int i = 2; i <= m; i++)
-    for (int j = 2; j <= n; j++) path[i][j] = path[i - 1][j] + path[i][j - 1];
-  return path[m][n];
+using namespace std;
+typedef long long ll;
+const int MAXN = 1000100;
+
+int W, H, L, U, R, D;
+double lfac[MAXN];
+double p2[MAXN];
+
+double choose(int N, int K) {
+  if (K < 0 || K > N) return 0;
+  return exp(lfac[N] - lfac[K] - lfac[N - K] - p2[N]);
 }
 
-int validPaths(int w, int h, int l, int u, int r, int d) {
-  vector<vector<int>> path(w + 1, vector<int>(h + 1, 1));
-  for (int i = 2; i <= w; i++)
-    for (int j = 2; j <= h; j++) {
-      if ((i >= l and i <= r) or (j >= u and j <= d))
-        path[i][j] = 0;
-      else
-        path[i][j] = path[i - 1][j] + path[i][j - 1];
+void gogo() {
+  cin >> W >> H >> L >> U >> R >> D;
+  W--, H--, L--, U--, R--, D--;
+
+  double ans = 0.0;
+
+  int ly = U, lx = R;
+  if (R < W) {
+    double mult = 1.0;
+    while (ly > 0) {
+      ly--;
+      lx++;
+
+      if (lx >= W) {
+        lx = W - 1;
+        mult = 0.5;
+      }
+      // cout << mult << lx << ly << "\n";
+      ans += mult * choose(lx + ly, lx);
     }
-  return path[w][h];
+  }
+
+  swap(U, L);
+  swap(D, R);
+  swap(H, W);
+  ly = U, lx = R;
+  if (R < W) {
+    double mult = 1.0;
+    while (ly > 0) {
+      ly--;
+      lx++;
+
+      if (lx >= W) {
+        lx = W - 1;
+        mult = 0.5;
+      }
+      ans += mult * choose(lx + ly, lx);
+    }
+  }
+  cout << fixed << setprecision(9);
+  cout << ans << "\n";
 }
 
 int main() {
-  int cases;
-  cin >> cases;
-  for (int k = 0; k < cases; k++) {
-    int w, h, l, u, r, d;
-    cin >> w >> h >> l >> u >> r >> d;
+  ios_base::sync_with_stdio(0);
 
-    int valid = validPaths(w, h, l, u, r, d);
-    int all = uniquePaths(w, h);
+  lfac[0] = 0.0;
+  for (int i = 1; i < MAXN; i++) lfac[i] = lfac[i - 1] + log(i);
+  p2[1] = log(2);
+  for (int i = 2; i < MAXN; i++) p2[i] = p2[i - 1] + p2[1];
 
-    cout << "case #" << k + 1 << ": " << valid << "/" << all << endl;
+  // cout << choose (1, 0) << "\n";
+
+  int T;
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    cout << "Case #" << i + 1 << ": ";
+    gogo();
   }
-
-  return 0;
 }
