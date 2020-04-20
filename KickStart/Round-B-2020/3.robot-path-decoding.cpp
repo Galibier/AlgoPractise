aa43dd1cac43f0c6af2b48a311e4698380451bdf
Galibier <cx.sjtu@gmail.com>
2020-04-20 23:21:34 +0800

    replace int with long long

--- KickStart/Round-B-2020/3.robot-path-decoding.cpp
+++ KickStart/Round-B-2020/3.robot-path-decoding.cpp
@@ -1,63 +1,65 @@
 #include <iostream>
 #include <map>
+#include <stack>
 #include <string>
 #include <vector>
 
 using namespace std;
-const int bound = 1000000000;
-int index = 0;
+const int inf = 1000000000;
 
-vector<int> op(string str) {
-  vector<int> ret = {0, 0, 0, 0};
-  for (index; index < str.size(); index++) {
-    if (isdigit(str[index])) {
-      int cnt = 0;
-      while (isdigit(str[index])) {
-        cnt = cnt * 10 + int(str[index] - '0');
-        index++;
-      }
-      index++;
-      auto res = op(str);
-      for (int j = 0; j < 4; j++) {
-        ret[j] += res[j] * cnt;
-      }
-      continue;
-    }
-    switch (str[index]) {
-      case 'N':
-        ret[0]++;
-        break;
-      case 'S':
-        ret[1]++;
-        break;
-      case 'E':
-        ret[2]++;
-        break;
-      case 'W':
-        ret[3]++;
-        break;
-      case ')':
-        return ret;
-    }
-  }
-  return ret;
-}
+struct Point {
+  long long cnt;
+  long long x;
+  long long y;
+  Point() : cnt(0), x(0), y(0) {}
+};
+
+stack<Point> st;
 
 int main() {
-  int cases;
+  long long cases;
   cin >> cases;
   for (int k = 0; k < cases; k++) {
     string input;
     cin >> input;
 
-    index = 0;
-    auto ans = op(input);
-    int south = (ans[1] - ans[0] + 1);
-    int east = (ans[2] - ans[3] + 1);
-    if (south < 1) south += bound;
-    if (east < 1) east += bound;
+    long long x = 0, y = 0;
+    for (int i = 0; i < input.size(); i++) {
+      if (isdigit(input[i])) {
+        Point t;
+        t.cnt = (long long)(input[i] - '0');
+        i++;
+        t.x = x;
+        t.y = y;
+        st.push(t);
+        continue;
+      }
+      switch (input[i]) {
+        case 'N':
+          y--;
+          break;
+        case 'S':
+          y++;
+          break;
+        case 'E':
+          x++;
+          break;
+        case 'W':
+          x--;
+          break;
+        case ')':
+          Point t = st.top();
+          st.pop();
+          x += (t.cnt - 1) * (x - t.x);
+          x %= inf;
+          y += (t.cnt - 1) * (y - t.y);
+          y %= inf;
+      }
+    }
+    x = (x + inf) % inf;
+    y = (y + inf) % inf;
 
-    cout << "case #" << k + 1 << ": " << east << " " << south << endl;
+    cout << "case #" << k + 1 << ": " << x + 1 << " " << y + 1 << endl;
   }
 
   return 0;
