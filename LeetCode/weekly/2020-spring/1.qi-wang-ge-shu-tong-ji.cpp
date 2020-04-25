4d0350a9ac48e8c2ba264990cf05c5d0418507ce
Galibier <cx.sjtu@gmail.com>
2020-04-25 16:02:03 +0800

    spring init

---
+++ LeetCode/weekly/2020-spring/1.qi-wang-ge-shu-tong-ji.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int expectNumber(vector<int>& scores) {
+        set<int> st(scores.begin(), scores.end());
+        return st.size();
+    }
+};
---
+++ LeetCode/weekly/2020-spring/2.xiao-zhang-shua-ti-ji-hua.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+
+class Solution {
+public:
+    int minTime(vector<int>& time, int m) {
+
+    }
+};
