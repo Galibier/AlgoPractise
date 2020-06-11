22c97a49e695f9a52101342709d29a13ad81279d
galibier <cx.sjtu@gmail.com>
2020-06-12 01:00:52 +0800

    first-missing-positive

--- LeetCode/cpp/41.first-missing-positive.cpp
+++ LeetCode/cpp/41.first-missing-positive.cpp
@@ -1,27 +1,32 @@
-/*
- * @lc app=leetcode id=41 lang=cpp
- *
- * [41] First Missing Positive
- */
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// @lc code=start
 class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums)
-    {
-        for (int i = 0; i < nums.size(); i++) {
-            if (i + 1 == nums[i])
-                continue;
-            int x = nums[i];
-            while (x >= 1 && x <= nums.size() && x != nums[x - 1]) {
-                swap(x, nums[x - 1]);
-            }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            if (i + 1 != nums[i])
-                return i + 1;
-        }
-        return nums.size() + 1;
+ public:
+  int firstMissingPositive(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+      if (i + 1 == nums[i]) continue;
+      int x = nums[i];
+      while (x >= 1 && x <= nums.size() && x != nums[x - 1]) {
+        swap(x, nums[x - 1]);
+      }
     }
+    cout << endl;
+    for (int i = 0; i < nums.size(); i++) {
+      if (i + 1 != nums[i]) return i + 1;
+    }
+    return nums.size() + 1;
+  }
 };
-// @lc code=end
+
+int main() {
+  Solution sol = Solution();
+  vector<int> nums = {1, 2, 0};  // 3
+  cout << sol.firstMissingPositive(nums) << endl;
+  nums = {3, 4, -1, 1};  // 2
+  cout << sol.firstMissingPositive(nums) << endl;
+  nums = {7, 8, 9, 11, 12};  // 1
+  cout << sol.firstMissingPositive(nums) << endl;
+  return 0;
+}
