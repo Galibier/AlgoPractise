d69c735e006469e7d683af32913910289c3b82d6
galibier <cx.sjtu@gmail.com>
2020-06-30 01:03:05 +0800

    weekly 194

---
+++ LeetCode/weekly/weekly-contest-194/1488.find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> findCriticalAndPseudoCriticalEdges(
+      int n, vector<vector<int>>& edges) {}
+};
+
+int main() {
+  Solution sol = Solution();
+  return 0;
+}
