2022157eaee0b1450d2ba89bba58320444902180
galibier <cx.sjtu@gmail.com>
2020-07-07 00:32:20 +0800

    sunday && kmp

--- LeetCode/cpp/28.implement-strstr.cpp
+++ LeetCode/cpp/28.implement-strstr.cpp
@@ -46,6 +46,32 @@ class Solution {
   }
 };
 
+class SolutionKMP {
+ public:
+  int strStr(string haystack, string needle) {
+    if (!needle.size()) return 0;
+    if (!haystack.size()) return -1;
+    int j = -1, i = 0;
+    vector<int> b(needle.size() + 1);
+    b[i] = j;
+    while (i < needle.size()) {
+      while (j >= 0 && needle[i] != needle[j]) j = b[j];
+      i++, j++;
+      b[i] = j;
+    }
+
+    j = 0, i = 0;
+    while (j < haystack.size()) {
+      while (i >= 0 && needle[i] != haystack[j]) i = b[i];
+      i++, j++;
+      if (i == needle.size()) {
+        return j - needle.size();
+      }
+    }
+    return -1;
+  }
+};
+
 int main() {
   Solution sol = Solution();
   string haystack = "hello", needle = "ll";
