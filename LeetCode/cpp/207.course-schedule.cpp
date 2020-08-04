611d8103aeb61d7b335e3fff7f93e0234070c72e
galibier <cx.sjtu@gmail.com>
2020-08-04 22:28:06 +0800

    daily

--- LeetCode/cpp/207.course-schedule.cpp
+++ LeetCode/cpp/207.course-schedule.cpp
@@ -1,9 +1,41 @@
-/*
- * @lc app=leetcode id=207 lang=cpp
- *
- * [207] Course Schedule
- */
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
 class Solution {
  public:
-  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {}
+  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> degrees(numCourses);
+    vector<vector<int>> adjacents(numCourses);
+    queue<int> zero;
+    int num = numCourses;
+    for (int i = 0; i < prerequisites.size(); i++) {
+      degrees[prerequisites[i][0]]++;
+      adjacents[prerequisites[i][1]].push_back(prerequisites[i][0]);
+    }
+    for (int i = 0; i < numCourses; i++)
+      if (degrees[i] == 0) {
+        zero.push(i);
+        num--;
+      }
+
+    while (!zero.empty()) {
+      int temp = zero.front();
+      zero.pop();
+      for (int j = 0; j < adjacents[temp].size(); j++)
+        if (--degrees[adjacents[temp][j]] == 0) {
+          zero.push(adjacents[temp][j]);
+          num--;
+        }
+    }
+    if (num == 0) return true;
+    return false;
+  }
 };
+
+int main() {
+  Solution sol;
+  return 0;
+}
