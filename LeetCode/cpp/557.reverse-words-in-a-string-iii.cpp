bfb2b227854ab68421c9a0865dc35d9ff419204a
galibier <cx.sjtu@gmail.com>
2020-08-30 01:08:54 +0800

    daily

---
+++ LeetCode/cpp/557.reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  string reverseWords(string s) {
+    int length = s.length();
+    int i = 0;
+    while (i < length) {
+      int start = i;
+      while (i < length && s[i] != ' ') {
+        i++;
+      }
+
+      int left = start, right = i - 1;
+      while (left < right) {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+      }
+      while (i < length && s[i] == ' ') {
+        i++;
+      }
+    }
+    return s;
+  }
+};
+
+int main() {
+  Solution sol;
+  return 0;
+}
