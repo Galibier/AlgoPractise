9110d57a0ea09ff2fc9a13913f88d0eb9725f3b4
Galibier <cx.sjtu@gmail.com>
2021-05-11 21:57:14 +0800

    0511

--- LeetCode/cpp/32.longest-valid-parentheses.cpp
+++ LeetCode/cpp/32.longest-valid-parentheses.cpp
@@ -9,6 +9,30 @@ using namespace std;
 class Solution {
  public:
   int longestValidParentheses(string s) {
+    vector<int> dp(s.size(), 0);
+    int maxVal = 0;
+    for (int i = 1; i < s.size(); i++) {
+      if (s[i] == ')') {
+        if (s[i - 1] == '(') {
+          dp[i] = 2;
+          if (i >= 2) {
+            dp[i] = dp[i - 2] + dp[i];
+          }
+        } else if (dp[i - 1] > 0) {
+          if (i - dp[i - 1] - 1 >= 0 && s[i - dp[i - 1] - 1] == '(') {
+            dp[i] = dp[i - 1] + 2;
+            if (i - dp[i - 1] - 2 > 0) {
+              dp[i] = dp[i] + dp[i - dp[i - 1] - 2];
+            }
+          }
+        }
+      }
+      maxVal = max(maxVal, dp[i]);
+    }
+    return maxVal;
+  }
+
+  int longestValidParentheses1(string s) {
     int cnt = 0;
     vector<int> dp(s.size() + 1, 0);
 
--- LeetCode/cpp/42.trapping-rain-water.cpp
+++ LeetCode/cpp/42.trapping-rain-water.cpp
@@ -1,14 +1,10 @@
-/*
- * @lc app=leetcode id=42 lang=cpp
- *
- * [42] Trapping Rain Water
- */
-
-// @lc code=start
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class Solution {
  public:
-  int trap(vector<int>& height) {
+  int trap1(vector<int>& height) {
     int left = 0, right = height.size() - 1;
     int res = 0;
     int maxleft = 0, maxright = 0;
@@ -29,5 +25,31 @@ class Solution {
     }
     return res;
   }
+
+  int trap2(vector<int> height) {
+    int sum = 0;
+    vector<int> max_left(height.size(), 0);
+    vector<int> max_right(height.size(), 0);
+
+    for (int i = 1; i < height.size() - 1; i++) {
+      max_left[i] = max(max_left[i - 1], height[i - 1]);
+    }
+    for (int i = height.size() - 2; i >= 0; i--) {
+      max_right[i] = max(max_right[i + 1], height[i + 1]);
+    }
+    for (int i = 1; i < height.size() - 1; i++) {
+      int minHeight = min(max_left[i], max_right[i]);
+      if (minHeight > height[i]) {
+        sum = sum + (minHeight - height[i]);
+      }
+    }
+    return sum;
+  }
 };
-// @lc code=end
+
+int main() {
+  Solution sol;
+  vector<int> vec{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+  cout << sol.trap2(vec) << endl;
+  return 0;
+}
